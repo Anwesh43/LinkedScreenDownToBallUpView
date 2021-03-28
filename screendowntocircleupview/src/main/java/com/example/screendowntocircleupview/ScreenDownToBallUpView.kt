e960d5b0e1ac1bbd51c3c8e44bdd17ce6891c3bf
Anwesh43 <anweshthecool0@gmail.com>
2021-03-29 01:29:29 +0530

    adding SDTBUNode class

--- screendowntocircleupview/src/main/java/com/example/screendowntocircleupview/ScreenDownToBallUpView.kt
+++ screendowntocircleupview/src/main/java/com/example/screendowntocircleupview/ScreenDownToBallUpView.kt
@@ -115,4 +115,45 @@ class ScreenDownToBallUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SDTBUNode(var i : Int, val state : State = State()) {
+
+        private var next : SDTBUNode? = null
+        private var prev : SDTBUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SDTBUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSDTBUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SDTBUNode {
+            var curr : SDTBUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
