ee08e18f68f4cf81b89118fa5173e484eb45a7c0
Anwesh43 <anweshthecool0@gmail.com>
2021-03-29 01:23:19 +0530

    adding Canvas extension methods

--- screendowntocircleupview/src/main/java/com/example/screendowntocircleupview/ScreenDownToBallUpView.kt
+++ screendowntocircleupview/src/main/java/com/example/screendowntocircleupview/ScreenDownToBallUpView.kt
@@ -29,3 +29,26 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawScreenDownToBallUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / rFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    drawRect(RectF(0f, h * sf2, w, h * sf1), paint)
+    save()
+    translate(w / 2, (h - size) - (h / 2 - size) * sf4)
+    drawArc(RectF(-size, -size, size, size), 0f, deg * sf3, true, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawSDTBUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawScreenDownToBallUp(scale, w, h, paint)
+}
